9d259b9e5489cb01823d72806c79223d881a1add
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-05-21 23:07:35 +0200

    shorter version

---
+++ Mtools/R/locations2.R
@@ -0,0 +1,9 @@
+#' Lon/Lat Coordinates for Zip-Codes
+#'
+#' Converted from http://download.geonames.org/export/zip/allCountries.zip
+#' See http://www.geonames.org/ for more.
+#'
+#' @docType data
+#' @name locations2
+#' @usage data(locations2)
+NULL
