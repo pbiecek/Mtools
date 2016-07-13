226004057bcd03688f256626bfa036c410d04424
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-07-13 12:56:27 +0200

    a new function

---
+++ Mtools/R/plotProfile.R
@@ -0,0 +1,14 @@
+#' Plot profile with vioplots
+#'
+#' This function ggplots vioplots for few variables and mark
+#' interesting points on these distributions
+#'
+#' @param data A data.frame in the long format with columns id, variable, value
+#' @param uid an interesting id to be marked
+#'
+#' @return gg object
+#'
+#' @export
+plotProfile <- function(data, uid) {
+
+}
