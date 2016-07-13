18dcd656f2661998b3399a5e7c24dce0e0902197
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-07-13 13:02:54 +0200

    sort, scale, plot

--- Mtools/R/plotProfile.R
+++ Mtools/R/plotProfile.R
@@ -11,8 +11,20 @@
 #'
 #' @export
 plotProfile <- function(data, uid, sort.max = TRUE) {
-  if (sort.max) {
+  if (sort.max)
     data$variable <- reorder(data$variable, data$value, max, na.rm=TRUE)
+
+  selected <- data[data$id == uid,]
+  selected$quant <- 0
+  for (i in 1:nrow(selected)) {
+    selected$quant[i] <-
+      round(100*(mean(data[,as.character(selected$variable[i])] < selected$value[i], na.rm = TRUE) +
+                   mean(data[,as.character(selected$variable[i])] <= selected$value[i], na.rm = TRUE))/2,1)
   }
 
+  ggplot(data, aes(x=variable, value)) +
+    geom_violin(fill="black", scale="width") +
+    geom_point(data=selected, size=4, color="red") +
+    geom_text(data=selected, aes(label=paste0(quant, "%")), vjust=-0.6, hjust=.5, color="blue", size=5) +
+    theme_classic() + coord_flip() + xlab("") + ylab("")
 }
