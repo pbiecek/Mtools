9f84ebb6db58651108e546d7fc6ae9a681e61a68
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-07-14 00:47:08 +0200

    plot places

---
+++ Mtools/R/profile_localisation.R
@@ -0,0 +1,20 @@
+#' Plot zipcodes
+#'
+#' This function leafplots zipcodes
+#'
+#' @param data A list of data.frames with the 'media' format
+#' @param uid an interesting id to be marked
+#'
+#' @return gg object
+#'
+#' @export
+plotProfile_time <- function(data, uid) {
+  tmp <- data[[uid]]
+  tmp <- cbind(tmp, zipcode[tmp$determined_location_zip_code,])
+
+  leaflet() %>%
+    addProviderTiles("Stamen.TonerLite",
+                     options = providerTileOptions(noWrap = TRUE)
+    ) %>%
+    addMarkers(data = tmp[,c("latitude","longitude")])
+}
