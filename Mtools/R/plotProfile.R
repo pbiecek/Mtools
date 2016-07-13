07e723fcc72fbb361a453e2fe67a9a109d9866f0
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-07-13 14:29:10 +0200

    more colors

--- Mtools/R/plotProfile.R
+++ Mtools/R/plotProfile.R
@@ -24,11 +24,14 @@ plotProfile <- function(dataWide, uid, sort.max = TRUE, log=TRUE) {
                    mean(dataWide[,as.character(selected$variable[i])] <= selected$value[i], na.rm = TRUE))/2,1)
   }
 
-  pl <- ggplot(data, aes(x=variable+log, value)) +
-    geom_violin(fill="black", scale="width") +
-    geom_point(data=selected, size=4, color="red") +
-    geom_text(data=selected, aes(label=paste0(quant, "%")), vjust=-0.6, hjust=.5, color="blue", size=5) +
-    theme_classic() + coord_flip() + xlab("") + ylab("")
+  max_y <- max(data$value, na.rm = TRUE)
+  pl <- ggplot(data, aes(x=variable, value+log)) +
+    geom_violin(fill="grey", color="grey", scale="width") +
+    geom_point(data=selected, size=8, aes(color=quant), shape="I") +
+    geom_text(data=selected, aes(label=paste0(quant, "%"), color=quant, y=max_y, size=quant), vjust=0.5, hjust=.5) +
+    theme_classic() + coord_flip() + xlab("") + ylab("") + theme(axis.text.y = element_text(size=12), legend.position="none")+
+    scale_colour_gradient(low="green", high="red", limits=c(0,100)) +
+    ggtitle(paste("Profile for id:",uid))
 
   if (log) pl <- pl + scale_y_log10(breaks=1+10^c(0,2,4,6,8), labels=10^c(0,2,4,6,8))
 
